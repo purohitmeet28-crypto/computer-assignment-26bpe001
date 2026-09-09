dd77edd0794ead31ba12583e70ecb9a395f0f2b7
purohitmeet28-crypto <purohitmeet.28@gmail.com>
2026-09-09 12:14:48 +0530

    Add files via upload

---
+++ n.33 cpp meet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Loop 10 times to calculate and print the first 10 even numbers
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", 2 * i);
+    }
+    return 0;
+}
